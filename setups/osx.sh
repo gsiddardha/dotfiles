705b942abc6b85f1750864a6b5641f0b2318634e
Siddardha Garimella <gsiddardha@gmail.com>
2017-02-24 02:41:12 +0530

    Moved zsh to osx setups

--- setups/osx.sh
+++ setups/osx.sh
@@ -5,6 +5,7 @@ echo "Installing osx tools ..."
 # Install homebrew
 install_if_does_not_exist brew "/usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
 install_if_does_not_exist git "brew install git"
+install_if_does_not_exist zsh "brew install zsh zsh-completions"
 
 
 echo "Done ..."
