bd93ead2e36d8e046eb07514ceb03d3932eb1ee0
Siddardha Garimella <gsiddardha@gmail.com>
2017-02-24 03:51:00 +0530

    Cleaning up setup and functions

--- setup.sh
+++ setup.sh
@@ -2,21 +2,8 @@
 
 echo -e "Setting up your Macbook...\n"
 
-# Defining the install function
-install_if_does_not_exist() {
-	if hash $1 2> /dev/null; then
-		echo "$1 found"
-	else
-		echo "$1 not found"
-		if [ -z  "$2" ]; then
-			echo "Missing installation command. Please provide it as the second argument ..."
-		else
-			echo "Installing it using the supplied command - $s"
-			`$2`
-			echo "Finished installation ..."
-		fi
-	fi
-}
+# Loading libraries
+source ~/dotfiles/setups/functions.sh
 
 # Running the setups
 source ~/dotfiles/setups/osx.sh
--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+source ~/dotfiles/setups/functions.sh
+
+brew_install_if_does_not_exist tmux
