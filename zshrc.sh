121c7f6c528bfb01b92e75f8be0fb4ffdb7f8204
Siddardha Garimella <gsiddardha@swiggy.in>
2017-02-24 02:08:11 +0530

    main source file

---
+++ zshrc.sh
@@ -0,0 +1,11 @@
+#! /bin/zsh
+
+echo -e "Good day, Siddardha!"
+
+# Initialise env
+source ~/dotfiles/env.sh
+
+# Initialise styling
+source ~/dotfiles/style.sh
+
+echo -e "\nAll done ... Have fun!!! :-)"
